ee5c33703979c2cebb8bffda1224372798a8880f
zackkhaz1 <54860278+zackkhaz1@users.noreply.github.com>
2020-12-04 12:14:16 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import sys
 import sphinx
 import mock
 sys.path.insert(0, os.path.abspath('..'))
-MOCK_MODULES = ['numpy','pymol','pandas','argparse','pathlib']
+MOCK_MODULES = ['numpy','pandas','argparse','pathlib']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
